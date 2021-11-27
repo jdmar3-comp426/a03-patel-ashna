5132c895bd8705855709c02f1ac5c4b40d49e566
Ashna Patel <ashna@live.unc.edu>
2021-11-27 11:24:00 -0500

    someeven test

--- src/main.js
+++ src/main.js
@@ -1,2 +1,2 @@
-import {everyEven} from './spicy/spicy_9.js'; 
-console.log(everyEven([7, 1, 7, 1, 7, 1, 7], x => x === 7));
+import {someEven} from './spicy/spicy_9.js'; 
+console.log(someEven([5, 7, 1, 7, 1, 7, 1, 7], x => x === 7));
--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -126,7 +126,14 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
+    for (let i = 0; i < arr.length; i = i+2) {
+        if (test(arr[i]) == 1) {
+            return true;
+        } 
+        else {
+            return false;
+        }
+    }
 };
 
 
